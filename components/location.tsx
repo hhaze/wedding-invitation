00b9cc4c85cedb287b63faf7d51bf362654c3927
jhgu <jhgu.dev@gmail.com>
2022-08-25 16:28:59 +0900

    Modify phrase and add location

--- components/location.tsx
+++ components/location.tsx
@@ -1,9 +1,38 @@
 import React, {FunctionComponent} from 'react'
+import Image from 'next/image'
 
 export const Location: FunctionComponent = props => {
   return (
-    <div className='flex flex-col items-center justify-center'>
-      약도 자리
+    <div className='flex flex-col items-center justify-center py-6'>
+      <Image
+        src='/royal_location.png'
+        width={500}
+        height={400}
+      />
+      <div className='w-full text-base text-left py-2'>
+        <div className='font-bold text-lg'>지하철</div>
+        <div>2호선 을지로입구역 하차 5번 출구 (도보 5분 소요)</div>
+        <div>4호선 명동역 하차 8번 출구 (도보 5분 소요)</div>
+      </div>
+      <div className='w-full text-base text-left py-2'>
+        <div className='font-bold text-lg'>버스</div>
+        <div>남대문세무서.서울백병원 하차 (도보 4분)</div>
+        <div>간선<span className='text-blue-600'>(파랑)</span> 470, 472, 741</div>
+        <div>직행<span className='text-red-600'>(빨강)</span> 1150, 3201, 4108, 5000A, 5000B 외 다수</div>
+        <div>광역<span className='text-red-600'>(빨강)</span> 9401, M4101, M4102, M4108 외 다수</div>
+      </div>
+      <div className='w-full text-base text-left py-2'>
+        <div className='font-bold text-lg'>자가용</div>
+        <div>· 명동성당주차장 이용 ( <span className='underline'>2시간 무료</span> )</div>
+        <div className='indent-3'>주소: 서울특별시 중구 명동길 74</div>
+        <div>· 주차장 출구에서 왼편으로 20미터 로얄호텔서울.</div>
+        <div className='indent-3'>호텔주차장 및 명동성당주차장 이용시</div>
+        <div className='indent-3'>KEB하나은행 옆길로 진입하여 주시기 바랍니다.</div>
+      </div>
+      <div className='w-full text-base text-left py-2'>
+        <div className='font-bold text-lg'>주소</div>
+        <div>서울특별시 중구 명동길 61 / T. 02-756-1112</div>
+      </div>
     </div>
   )
 }
--- components/phrase.tsx
+++ components/phrase.tsx
@@ -3,7 +3,7 @@ import React, {FunctionComponent} from 'react'
 export const Phrase: FunctionComponent = props => {
   return (
     <div className='flex flex-col items-center justify-center'>
-      <div className='text-xl text-yellow-500 py-6'>
+      <div className='text-xl text-rose-300 font-bold py-6'>
         소중한 분들을 초대합니다.
       </div>
       <div className='flex flex-col items-center text-base'>
@@ -11,21 +11,21 @@ export const Phrase: FunctionComponent = props => {
         <span>결혼합니다!</span>
       </div>
       <div className='py-6'>
-        <div className='flex justify-center text-lg font-bold'>
+        <div className='flex justify-center text-lg'>
           <span className='px-1'>이규석</span>
           <span className='px-1'>·</span>
           <span className='px-1'>이해자</span>
-          <span className='px-1 text-sm'>의</span>
-          <span className='px-1 text-sm w-10'>아들</span>
-          <span className='px-1'>봉균</span>
+          <span className='px-1 text-sm py-1'>의</span>
+          <span className='px-1 text-sm w-10 py-1'>아들</span>
+          <span className='px-1 font-bold'>봉균</span>
         </div>
-        <div className='flex justify-center text-lg font-bold'>
+        <div className='flex justify-center text-lg'>
           <span className='px-1'>구성종</span>
           <span className='px-1'>·</span>
           <span className='px-1'>홍남숙</span>
-          <span className='px-1 text-sm'>의</span>
-          <span className='px-1 text-sm w-10'>딸</span>
-          <span className='px-1'>지혜</span>
+          <span className='px-1 text-sm py-1'>의</span>
+          <span className='px-1 text-sm w-10 py-1'>딸</span>
+          <span className='px-1 font-bold'>지혜</span>
         </div>
       </div>
     </div>
