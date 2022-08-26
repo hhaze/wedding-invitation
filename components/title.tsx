a40fec8eda635bb12cffe05998cd165da33e0fce
jhgu <jhgu.dev@gmail.com>
2022-08-26 18:42:23 +0900

    Add title component

---
+++ components/title.tsx
@@ -0,0 +1,21 @@
+import React, {FunctionComponent} from 'react'
+import Image from 'next/image'
+
+export const Title: FunctionComponent = () => {
+  return (
+    <>
+      <div className='py-8 text-2xl'>
+        이봉균 | 구지혜
+      </div>
+      <Image
+        src="https://images.unsplash.com/photo-1520854221256-17451cc331bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+        width={400}
+        height={600}
+      />
+      <div className='flex flex-col items-center py-8 text-lg'>
+        <span>2022년 09월 24일 토요일 낮 12시</span>
+        <span>로얄호텔서울 3층 그랜드 볼룸</span>
+      </div>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react'
 import type { NextPage } from 'next'
-import Image from 'next/image'
+import {Title} from '../components/title'
 import {Phrase} from '../components/phrase'
+import {Photo} from '../components/photo'
 import {Calendar} from '../components/calendar'
 import {Location} from '../components/location'
 import {Banking} from '../components/banking'
 import {Rsvp} from '../components/rsvp'
-import {Photo} from '../components/photo'
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center font-['Noto+Serif+KR']">
-      <div className='py-8 text-2xl'>
-        이봉균 | 구지혜
-      </div>
-      <Image
-        src="https://images.unsplash.com/photo-1520854221256-17451cc331bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
-        width={400}
-        height={600}
-      />
-      <div className='flex flex-col items-center py-8 text-lg'>
-        <span>2022년 09월 24일 토요일 낮 12시</span>
-        <span>로얄호텔서울 3층 그랜드 볼룸</span>
-      </div>
+      <Title/>
       <Phrase/>
       <Photo/>
       <Calendar/>
